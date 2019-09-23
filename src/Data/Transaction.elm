ad14a8b0bd9c43847da8e2c1b346f390d2823664
Marcin Bilski <gyamtso@gmail.com>
2019-09-23 21:35:34 +0200

    Remove unused function.

--- src/Data/Transaction.elm
+++ src/Data/Transaction.elm
@@ -1,4 +1,4 @@
-module Data.Transaction exposing (LedgerEntry, Split, Transaction, TransactionId, decodeLedgerEntry, decodeSplit, decodeTransaction, setDescription)
+module Data.Transaction exposing (LedgerEntry, Split, Transaction, TransactionId, decodeLedgerEntry, decodeSplit, decodeTransaction)
 
 import Data.Account exposing (Account)
 import Dict exposing (Dict)
@@ -62,11 +62,6 @@ type alias SplitId =
 
 type alias AccountId =
     String
-
-
-setDescription : String -> Transaction -> Transaction
-setDescription newDescription transaction =
-    { transaction | description = newDescription }
 
 
 decodeTransaction : Dict AccountId Account -> Decode.Decoder Transaction
